32bb61f835e39e428db35654cf978a9ad4ac1e4c
Zargon <shivammawasare@gmail.com>
2025-07-05 02:14:50 +0530

    added elastic ip

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://13.234.140.163/api' || 'http://localhost:5000/api',
+  baseURL: 'http://13.234.140.163:5000/api' || 'http://localhost:5000/api',
 });
 
 axiosInstance.interceptors.request.use(
